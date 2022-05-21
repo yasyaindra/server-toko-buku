08fefcb79b792dd4944c484df0ad785434d0ad2f
yasyaindra <indrayasya96@gmail.com>
2022-05-21 14:07:42 +0800

    add origin:*

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
-
 const authRouter = require("./app/api/auth/router");
 const categoriesRouter = require("./app/api/categories/router");
 const booksRouter = require("./app/api/books/router");
@@ -11,9 +10,10 @@ const uploadsRouter = require("./app/api/uploads/router");
 const checkoutRouter = require("./app/api/checkout/router");
 const transactionRouter = require("./app/api/transactions/router");
 const cors = require("cors");
-const URL = "/api/v1";
 
 const app = express();
+const URL = "/api/v1";
+app.use(cors());
 
 app.use(
   session({
@@ -28,11 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Server Toko Buku" });
