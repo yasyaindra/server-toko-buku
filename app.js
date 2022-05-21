c2f230abc0f6be29f733b3ce2745618b2c7c6a75
yasyaindra <indrayasya96@gmail.com>
2022-05-21 13:43:18 +0800

    cors edit position

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const URL = "/api/v1";
 
 const app = express();
 
-app.use(cors());
 app.use(
   session({
     secret: "keyboard cat",
@@ -29,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Server Toko Buku" });
